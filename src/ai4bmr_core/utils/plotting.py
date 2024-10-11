1222875d722853a31f3daa14cc9ab6cab6a6c765
adrianomartinelli <adrianom@student.ethz.ch>
2024-10-11 09:17:44 +0200

    add examples for legend placement

--- src/ai4bmr_core/utils/plotting.py
+++ src/ai4bmr_core/utils/plotting.py
@@ -1,4 +1,18 @@
 def legend_from_dict(label_to_color: dict):
+    """Create a legend from a dict with labels as keys and colors as values.
+    See: https://matplotlib.org/stable/users/explain/axes/legend_guide.html#legend-guide
+
+    Example:
+        # place legend outside the ax
+        cg.ax_heatmap.legend(handles=legend_elements, loc='upper left', bbox_to_anchor=(1.05, 1), fontsize=6)
+        # place legend in the top right corner of the figure
+        cg.ax_heatmap.legend(handles=legend_elements, bbox_to_anchor=(1, 1), bbox_transform=cg.ax_heatmap.figure.transFigure, fontsize=6)
+    Args:
+        label_to_color:
+
+    Returns:
+
+    """
     from matplotlib.patches import Patch
 
     legend_elements = [Patch(facecolor=color, label=label) for label, color in label_to_color.items()]
