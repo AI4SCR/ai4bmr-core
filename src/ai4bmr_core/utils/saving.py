67d5505255ce85b832781118af537719a8f03c2b
adrianomartinelli <adrianom@student.ethz.ch>
2025-01-23 13:45:59 +0100

    switch to tifffile

--- src/ai4bmr_core/utils/saving.py
+++ src/ai4bmr_core/utils/saving.py
@@ -1,19 +1,21 @@
 from pathlib import Path
 
 import numpy as np
+import tifffile
 
 
 def save_mask(mask: np.ndarray, save_path: Path):
-    from skimage.io import imsave
+    assert mask.dtype in ["uint32", "uint16"]
 
     if mask.max() < 65536:
         mask = mask.astype("uint16")
     else:
         mask = mask.astype("uint32")
-    imsave(save_path, mask, check_contrast=False, plugin="tifffile", compression="deflate")
+
+    tifffile.imwrite(save_path, mask, compression="deflate")
 
 
 def save_image(img: np.ndarray, save_path: Path):
-    from skimage.io import imsave
+    import tifffile
 
-    imsave(save_path, img, plugin="tifffile", compression="deflate")
+    tifffile.imwrite(save_path, img, compression="deflate")
